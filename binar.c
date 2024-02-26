21725c34d76a198c061c2a9e933c217de5730f19
thomasbebbington <thomasbebbington1607@gmail.com>
2024-02-26 19:13:26 +0000

    added complete board check

--- binar.c
+++ binar.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define rows 6
@@ -19,7 +20,113 @@ void drawGameGrid(int* grid){
 	}
 }
 
-int done(int grid[rows][columns]){
+char checkDone(int grid[rows][columns]){
+	int countred;
+	int countblue;
+	//check number of colours in rows
+	for(int i = 0; i < rows; i++){
+		countred = 0;
+		countblue = 0;
+		for(int j = 0; j < columns; j++){
+			if(grid[j][i] == 1){
+				countred++;
+			} else {
+				countblue++;
+			}
+		}
+		if(abs(countred - countblue) > 1){
+			return 0;
+		}
+	}
+
+	//check number of colours in columns
+	for(int i = 0; i < columns; i++){
+		countred = 0;
+		countblue = 0;
+		for(int j = 0; j < rows; j++){
+			if(grid[i][j] == 1){
+				countred++;
+			} else {
+				countblue++;
+			}
+		}
+		if(abs(countred - countblue) > 1){
+			return 0;
+		}
+	}
+
+	
+	
+	//check for conescutive colours in rows
+	int current;
+	int count;
+	for(int i = 0; i < rows; i++){
+		current = grid[0][i];
+		count = 1;
+		for(int j = 1; j < columns; j++){
+			if(grid[j][i] == current){
+				count++;
+			} else {
+				current = grid[j][i];
+				count = 1;
+			}
+			if(count > 2){
+				printf("Row %d has too many consecutive colours\n", i);
+				return 0;
+			}
+		}
+	}
+	
+	
+	//check for consecutive colours in columns
+	for(int i = 0; i < columns; i++){
+		current = grid[i][0];
+		count = 1;
+		for(int j = 1; j < rows; j++){
+			if(grid[i][j] == current){
+				count++;
+			} else {
+				current = grid[i][j];
+				count = 1;
+			}
+			if(count > 2){
+				printf("Column %d has too many consecutive colours\n", i);
+				return 0;
+			}
+		}
+	}
+	
+	//check uniqueness of columns	
+	for(int i = 0; i < rows - 1; i++){
+		for(int j = i + 1; j < rows; j++){
+			for(int k = 0; k < columns; k++){
+				if(grid[i][k] != grid[j][k]){
+					break;
+				}
+				if(k == columns - 1){
+					printf("Column %d = Column %d\n", i, j);
+					return 0;
+				}
+			}
+		}
+	}
+	
+	//check uniqueness of rows
+	for(int i = 0; i < columns - 1; i++){
+		for(int j = i + 1; j < columns; j++){
+			for(int k = 0; k < rows; k++){
+				if(grid[k][i] != grid[k][j]){
+					break;
+				}
+				if(k == columns - 1){
+					printf("Row %d = Row %d\n", i, j);
+					return 0;
+				}
+			}
+		}
+	}
+	
+
 	return 1;
 }
 
@@ -48,9 +155,9 @@ void main(){
 		DrawFPS(10,10);
 
 		if(filledSquares == rows * columns){
-			if(done(grid)){
-				DrawText("FINISHED???", 400, 400, 30, DARKBLUE);
-			}
+			DrawText("FINISHED???", 400, 400, 30, DARKBLUE);
+
+			printf("%d\n", checkDone(grid));
 		}
 
 		if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
