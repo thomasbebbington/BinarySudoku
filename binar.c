59fccd6d41ec76596f79ad2e167df5749c6adf6b
thomasbebbington <thomasbebbington1607@gmail.com>
2024-03-03 13:11:37 +0000

    Moved checkDone function to header file

--- binar.c
+++ binar.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "binar.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -20,111 +21,9 @@ void drawGameGrid(int* grid){
 	}
 }
 
-char checkDone(int grid[rows][columns]){
-	int countred;
-	int countblue;
-	//check number of colours in rows
-	for(int i = 0; i < rows; i++){
-		countred = 0;
-		countblue = 0;
-		for(int j = 0; j < columns; j++){
-			if(grid[j][i] == 1){
-				countred++;
-			} else {
-				countblue++;
-			}
-		}
-		if(abs(countred - countblue) > 1){
-			return 0;
-		}
-	}
-
-	//check number of colours in columns
-	for(int i = 0; i < columns; i++){
-		countred = 0;
-		countblue = 0;
-		for(int j = 0; j < rows; j++){
-			if(grid[i][j] == 1){
-				countred++;
-			} else {
-				countblue++;
-			}
-		}
-		if(abs(countred - countblue) > 1){
-			return 0;
-		}
-	}
-
 	
-	
-	//check for conescutive colours in rows
-	int current;
-	int count;
-	for(int i = 0; i < rows; i++){
-		current = grid[0][i];
-		count = 1;
-		for(int j = 1; j < columns; j++){
-			if(grid[j][i] == current){
-				count++;
-			} else {
-				current = grid[j][i];
-				count = 1;
-			}
-			if(count > 2){
-				return 0;
-			}
-		}
-	}
-	
-	
-	//check for consecutive colours in columns
-	for(int i = 0; i < columns; i++){
-		current = grid[i][0];
-		count = 1;
-		for(int j = 1; j < rows; j++){
-			if(grid[i][j] == current){
-				count++;
-			} else {
-				current = grid[i][j];
-				count = 1;
-			}
-			if(count > 2){
-				return 0;
-			}
-		}
-	}
-	
-	//check uniqueness of columns	
-	for(int i = 0; i < rows - 1; i++){
-		for(int j = i + 1; j < rows; j++){
-			for(int k = 0; k < columns; k++){
-				if(grid[i][k] != grid[j][k]){
-					break;
-				}
-				if(k == columns - 1){
-					return 0;
-				}
-			}
-		}
-	}
-	
-	//check uniqueness of rows
-	for(int i = 0; i < columns - 1; i++){
-		for(int j = i + 1; j < columns; j++){
-			for(int k = 0; k < rows; k++){
-				if(grid[k][i] != grid[k][j]){
-					break;
-				}
-				if(k == columns - 1){
-					return 0;
-				}
-			}
-		}
-	}
 	
 
-	return 1;
-}
 
 void main(){
 	const int width = 100*rows;
---
+++ binar.h
@@ -0,0 +1,118 @@
+#include<stdlib.h>
+#define rows 6
+#define columns 6
+
+char checkDone(int grid[rows][columns]){
+	int countred;
+	int countblue;
+	//check number of colours in rows
+	for(int i = 0; i < rows; i++){
+		countred = 0;
+		countblue = 0;
+		for(int j = 0; j < columns; j++){
+			if(grid[j][i] == 1){
+				countred++;
+			} else {
+				countblue++;
+			}
+		}
+		if(abs(countred - countblue) > 1){
+			return 0;
+		}
+	}
+
+	//check number of colours in columns
+	for(int i = 0; i < columns; i++){
+		countred = 0;
+		countblue = 0;
+		for(int j = 0; j < rows; j++){
+			if(grid[i][j] == 1){
+				countred++;
+			} else {
+				countblue++;
+			}
+		}
+		if(abs(countred - countblue) > 1){
+			return 0;
+		}
+	}
+
+	
+	
+	//check for conescutive colours in rows
+	int current;
+	int count;
+	for(int i = 0; i < rows; i++){
+		current = grid[0][i];
+		count = 1;
+		for(int j = 1; j < columns; j++){
+			if(grid[j][i] == current){
+				count++;
+			} else {
+				current = grid[j][i];
+				count = 1;
+			}
+			if(count > 2){
+				return 0;
+			}
+		}
+	}
+
+	//check for consecutive colours in columns
+	for(int i = 0; i < columns; i++){
+		current = grid[i][0];
+		count = 1;
+		for(int j = 1; j < rows; j++){
+			if(grid[i][j] == current){
+				count++;
+			} else {
+				current = grid[i][j];
+				count = 1;
+			}
+			if(count > 2){
+				return 0;
+			}
+		}
+	}
+	
+	//check uniqueness of columns	
+	for(int i = 0; i < rows - 1; i++){
+		for(int j = i + 1; j < rows; j++){
+			for(int k = 0; k < columns; k++){
+				if(grid[i][k] != grid[j][k]){
+					break;
+				}
+				if(k == columns - 1){
+					return 0;
+				}
+			}
+		}
+	}
+	
+	//check uniqueness of rows
+	for(int i = 0; i < columns - 1; i++){
+		for(int j = i + 1; j < columns; j++){
+			for(int k = 0; k < rows; k++){
+				if(grid[k][i] != grid[k][j]){
+					break;
+				}
+				if(k == columns - 1){
+					return 0;
+				}
+			}
+		}
+	}
+	
+
+	return 1;
+}
+
+void printGrid(int grid[rows][columns]){
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < columns; j++){
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+
+}
